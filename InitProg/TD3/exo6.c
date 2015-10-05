65441d6dd7ca8d316aea274978908a46e90dd15a
slydevis <slydevis@hotmail.fr>
2015-10-06 00:10:59 +0200

    Split InitInfo and InitProg

---
+++ InitProg/TD3/exo6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#define N 10
+
+int findDico(int tab[], int val) {
+    return 0;
+}
+
+int main() {
+    int tab[N] = {-2, -1, 3, 4, 5, 6, 7, 8, 9, 10};
+    int res = findDico(tab, -1);
+    printf("Le résultat est de %d\n", res);
+    return 0;
+}
