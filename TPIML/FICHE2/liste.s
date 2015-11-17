30a757ddcaf6922e8622fa8cc94f8d294c727c5e
Guillaume Michon <slydevis@hotmail.fr>
2015-11-17 13:37:00 +0100

    Correct fact.s and add liste.s and liste2.s

--- TPIML/FICHE2/liste.s
+++ TPIML/FICHE2/liste.s
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------
 #-- File name : somme.s
-#-- Objet : TP Interface MatŽriel Logiciel  - Fiche 2
+#-- Objet : TP Interface MatÂŽriel Logiciel  - Fiche 2
 #--
 #--     (Voir Cours, chapitre 2 - Langage d'Assemblage
 #--                 transparents SPIM : exemple        )
@@ -11,49 +11,38 @@
 # variables :
 #    SOMME, COMPTEUR : entiers 
 #    SOMME est en memoire et COMPTEUR dans $s0
-
-
-# Algo 
-# FACT = 50
-# RES = 0
-# while(FACT >= 1) {
-#   RES = RES * FACT
-#   FACT = FACT - 1;
-# }
-
+        .space 48703 
      	.align 2  # non necessaire 
-RES : .space 4  # ou .word 0 pour faire RES <--- 0
+SOMME : .space 4  # ou .word 0 pour faire SOMME <--- 0
+
 	.text
 	.globl main
 main :
 # debut :
 debut :
-    
-#   RES <---0
-    li $t1, 1
-	sw $t1, RES($0)
-#   FACT <--- 10
-	li $s0, 10
-#boucle: Si FACT < 1 aller a suite
-	li $t1, 1
+
+# 	  SOMME <---0
+	sw $0, SOMME($0)
+
+#boucle: Si COMPTEUR < 0 aller a suite
+	li $v0, 0
 boucle:	
 
-	blt $s0, $t1, suite
+	blt $v0, 0, suite
 #        sinon 
-#   RES <--- RES * FACT
-	lw $t2, RES($0)
-	mulo $t2,$t2, $s0
-	sw  $t2, RES($0)
-#   FACT <--- FACT -1 
-	addi $s0, $s0, -1
-#          aller a boucle
+# SAISIR UN NOMBRE
+    li $v0, 5
+    syscall
+    add $t2, $t2, $v0
+    sw $t2, SOMME($0)
+#   aller a boucle
 	beq $0, $0, boucle    # ou j boucle
 #        finsi
-#suite: afficher(RES)
+# suite: afficher(SOMME)
 suite:  
-# instructions pour afficher l'entier en mémoire à l'adresse RES($0)
+# instructions pour afficher l'entier en mÃ©moire Ã  l'adresse SOMME($0)
 	li $v0, 1    # system call code for print_int (see page A.49)
-	lw $a0, RES($0) # integer to print
+	lw $a0, SOMME($0) # integer to print
 	syscall      # print
 #fin
 	jr $ra         # Retour de la procedure main.
