c08ba2810ee832da2b5124c7c81f9e7ee3e10164
slydevis <slydevis@hotmail.fr>
2015-11-18 18:02:16 +0100

    Add TPListe (suite) without the last exercice

--- InitProg/TPListes/main.c
+++ InitProg/TPListes/main.c
@@ -150,7 +150,7 @@ int findListe(t_ptr_liste liste, int val) {
     }
     
     if(trouve == 0)
-        pos = 0;
+        pos = -1;
         
     return pos; 
 }
@@ -247,6 +247,120 @@ t_ptr_liste inversionListeADT(t_ptr_liste liste) {
     return buff;
 }
 
+/* TP LISTE SUITE */
+
+/* Exercice 1 : Une liste palindrome */
+
+/* Renvoi 1 si c'est un palindrome, 0 dans les autres cas */
+int isPalindrome(t_ptr_liste liste) {
+    int res = 1;
+    int med = sizeList(liste);
+    int cpt;
+    int i;
+    t_ptr_liste tmp = liste;
+    t_ptr_liste debutListe = liste;
+
+    if(med%2 == 0)
+        med = med/2;
+    else
+        med = (med+1)/2;
+
+    cpt = med-1;
+    for(i = 0; i < med; ++i)
+        tmp = tmp->next;
+
+    while(tmp != NULL && res == 1) {
+        for(i = 0; i < cpt-1; ++i)
+            debutListe = debutListe->next;
+    
+        if(tmp->value != debutListe->value)
+            res = 0;
+        tmp = tmp->next;
+        debutListe = liste;
+
+        cpt = cpt - 1;
+    }
+
+    return res;
+}
+
+/* Exercice 2 : Une liste de Fibonacci */
+
+int fib(int n) {
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
+
+    return fib(n-1) + fib(n-2);
+}
+
+/* Renvoie 1 si la liste est une liste de Fibonacci 0 sinon */
+int isFibonnacciList(t_ptr_liste liste) {
+    int i = 0, res = 1;
+    t_ptr_liste tmp = liste;
+
+    while(tmp != NULL && res == 1) {
+        if(tmp->value != fib(i))            
+            res = 0;
+
+        tmp = tmp->next;
+        i = i+1;
+    }
+
+    return res;
+}
+
+/* Exercice 3 : La fusion de deux liste triées */
+t_ptr_liste fusionList(t_ptr_liste liste, t_ptr_liste secondList) {
+    t_ptr_liste res = NULL;
+    t_ptr_liste listeTmp = liste;
+    t_ptr_liste secondListTmp = secondList;
+
+    while(listeTmp != NULL) {
+        res = addOrd(res, listeTmp->value);
+        listeTmp = listeTmp->next;        
+    }
+
+    while(secondListTmp != NULL) {
+        res = addOrd(res, secondListTmp->value);
+        secondListTmp = secondListTmp->next;
+    }
+
+    return res;
+}
+
+/* Exercice 4 : La fusion de deux liste triées sans répétition */
+t_ptr_liste fusionListNoRepeat(t_ptr_liste liste, t_ptr_liste secondList) {
+    t_ptr_liste res = NULL;
+    t_ptr_liste listeTmp = liste;
+    t_ptr_liste secondListTmp = secondList;
+
+    while(listeTmp != NULL) {
+        if(findListe(res, listeTmp->value) == -1) {
+            res = addOrd(res, listeTmp->value);
+        }
+        
+        listeTmp = listeTmp->next;        
+    }
+
+    while(secondListTmp != NULL) {
+        if(findListe(res, secondListTmp->value) == -1) {
+            res = addOrd(res, secondListTmp->value);
+        }
+
+        secondListTmp = secondListTmp->next;
+    }
+
+    return res;
+}
+
+/* Exercice 5 : La liste des sommes préfixe */
+
+t_ptr_liste sommeList(t_ptr_liste liste) {
+    return NULL;
+} 
+
 /* Libération des ressources */
 
 void freeListe(t_ptr_liste liste) {
@@ -260,7 +374,7 @@ void freeListe(t_ptr_liste liste) {
 }
 
 int main() {
-    /*Création d'une liste */
+    /* Création d'une liste */
     
     t_ptr_liste liste = creationListe();
     
@@ -298,13 +412,73 @@ int main() {
     printf("Inversion de la liste ordonnée :\n");
     listeOrd = inversionListeADT(listeOrd);
     afficher(listeOrd);
-    
+
+    /* Vérification si c'est un palindrome */
+    t_ptr_liste palin = NULL;
+    palin = ajout_liste(palin, 1);
+    palin = ajout_liste(palin, 2);
+    palin = ajout_liste(palin, 3);
+    palin = ajout_liste(palin, 4);
+    palin = ajout_liste(palin, 3);
+    palin = ajout_liste(palin, 2);
+    palin = ajout_liste(palin, 1);
+
+    printf("Palin :\n");
+    afficher(palin);
+    printf("La liste palin est un paladrome ? ");
+    (isPalindrome(palin) ? printf("Oui\n") : printf("Non\n"));
+
+    t_ptr_liste paPalin = NULL;
+    paPalin = ajout_liste(paPalin, 1);
+    paPalin = ajout_liste(paPalin, 2);
+    paPalin = ajout_liste(paPalin, 3);
+    paPalin = ajout_liste(paPalin, 4);
+    printf("Pas Palin :\n");
+    afficher(paPalin);
+    printf("La liste papalin est un paladrome ? ");
+    (isPalindrome(paPalin) ? printf("Oui\n") : printf("Non\n"));
+
+    /* Vérification que la liste est une suite de Fibonacci */
+    t_ptr_liste fibList = NULL;
+    fibList = ajout_liste(fibList, 3);
+    fibList = ajout_liste(fibList, 2);
+    fibList = ajout_liste(fibList, 1);
+    fibList = ajout_liste(fibList, 1);
+    fibList = ajout_liste(fibList, 0);
+    printf("fibList :\n");
+    afficher(fibList);
+    printf("La liste fibList est une liste de Fibonacci ? ");
+    (isFibonnacciList(fibList) ? printf("Oui\n") : printf("Non\n"));
+
+    /* Fusion de deux liste triée */
+    printf("Fusion de listeOrd et de paPalin\n");
+    t_ptr_liste testFusion = fusionList(listeOrd, paPalin);
+    afficher(testFusion);
+
+    printf("Fusion de listeOrd et de paPalin (sans répétition)\n");
+    testFusion = fusionListNoRepeat(listeOrd, paPalin);
+    afficher(testFusion);
+
+    /* Liste des sommes préfixe */
+    t_ptr_liste listeSomme = NULL;
+    listeSomme = ajout_liste(listeSomme, 3);
+    listeSomme = ajout_liste(listeSomme, 5);
+    listeSomme = ajout_liste(listeSomme, 4);
+    listeSomme = ajout_liste(listeSomme, 2);
+    listeSomme = ajout_liste(listeSomme, 1);
+    listeSomme = sommeList(listeSomme);
+    printf("La liste des sommes préfixe donne : \n");
+    afficher(listeSomme);
+
     /* Libération de la mémoire */
-    
     freeListe(listeOrd);
     freeListe(liste);
     freeListe(newList);
-
+    freeListe(palin);
+    freeListe(paPalin);
+    freeListe(fibList);
+    freeListe(testFusion);
+    freeListe(listeSomme);
     return 0;
 }
  
