a4900132c0b9fbfc31229ab4b394a9da5c2d4229
Guillaume Michon <slydevis@hotmail.fr>
2016-03-07 12:55:49 +0100

    Logique/TP1 : Update exo 2

--- Logique/TP1/exo2.cpp
+++ Logique/TP1/exo2.cpp
@@ -12,19 +12,40 @@ void init(unsigned long int &x) {
 
 // assigne a vrai le bit en position pos
 void setTrue(unsigned long int &x, int pos) {
+    if(pos > TAB_SIZE)
+        return;
+        
     unsigned long int tmp = 1;
-    for(int i = 0; i < pos; ++i)
-        tmp <<= 1;        
+    for(unsigned i = 0; i < pos; ++i)
+        tmp <<= 1;
+                
     x = x|tmp;
 }
 
 // assigne a faux le bit en position pos
 void setFalse(unsigned long int &x, int pos) {
-
+    if(pos > TAB_SIZE)
+        return;
+        
+    unsigned long int tmp = 1;
+    for(unsigned i = 0; i < pos; ++i)
+        tmp <<= 1;
+           
+    tmp = ~tmp;
+    x = x&tmp; 
 }
 
 // retourne la valeur du bit en position pos
-bool get(unsigned long int &x, int pos);
+bool get(unsigned long int &x, int pos) {
+    if(pos > TAB_SIZE)
+        return false;
+    
+    unsigned long int tmp = x;
+    for(unsigned i = 0; i < pos; ++i)
+        tmp >>= 1;
+        
+    return tmp;
+}
 
 int main() {
     cout << sizeof(unsigned long int)*8 << endl; // Renvoi 64 
@@ -38,5 +59,11 @@ int main() {
     bitset<64> xBinary(x);
     cout << "x = " << xBinary << endl;
     
+    cout << "x(3) = " << get(x, 3) << endl;
+    setTrue(x, 5);
+    cout << "x(5) = " << get(x, 5) << endl;
+    setFalse(x, 8);
+    cout << "x(8) = " << get(x, 8) << endl;
+ 
     return 0;
 }
